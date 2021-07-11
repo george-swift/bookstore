209b0aa0c7d8a92d7495fbe954462b83b08b4691
George <71434803+george-swift@users.noreply.github.com>
2021-07-11 16:12:25 +0100

    Initialize Redux store and add Provider context

--- src/index.js
+++ src/index.js
@@ -1,10 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import { uniqueID } from './constants';
+import App from './components/App';
+
+const books = [
+  {
+    id: uniqueID(),
+    title: 'The Martian',
+    category: 'Sci-Fi',
+  },
+  {
+    id: uniqueID(),
+    title: 'Hands-on Machine Learning',
+    category: 'Learning',
+  },
+  {
+    id: uniqueID(),
+    title: "Alice's Adventures in Wonderland",
+    category: 'Kids',
+  },
+  {
+    id: uniqueID(),
+    title: 'The Woman in Black',
+    category: 'Horror',
+  },
+  {
+    id: uniqueID(),
+    title: 'Black and British: A Forgotten History',
+    category: 'History',
+  },
+  {
+    id: uniqueID(),
+    title: 'Steve Jobs',
+    category: 'Biography',
+  },
+  {
+    id: uniqueID(),
+    title: 'The Hunger Games',
+    category: 'Action',
+  },
+];
+
+const initialState = { books };
+
+const store = createStore(rootReducer, initialState);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
