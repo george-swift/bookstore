d2e9888636a2d68c93669515e997b25d779ca160
George <71434803+george-swift@users.noreply.github.com>
2021-07-11 18:40:26 +0100

    Refactor form nodes

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -4,24 +4,20 @@ import { CATEGORIES } from '../constants';
 export default function BooksForm() {
   return (
     <form className="container row g-2 bookform">
-      <div className="col-lg-6">
-        <div className="form-floating">
-          <input type="text" className="form-control" placeholder="Book title" />
-          <label>Book title</label>
-        </div>
+      <div className="col-lg-6 form-floating">
+        <input type="text" className="form-control" placeholder="Book title" />
+        <label>Book Title</label>
       </div>
-      <div className="col-lg-4">
-        <div className="form-floating">
-          <select className="form-select">
-            <option defaultValue="Select a category">Select a category</option>
-            {
-              CATEGORIES.map((category) => (
-                <option key={category} value={category}>{category}</option>
-              ))
-            }
-          </select>
-          <label>Category</label>
-        </div>
+      <div className="col-lg-4 form-floating">
+        <select className="form-select">
+          <option defaultValue="Select a category">Select a category</option>
+          {
+            CATEGORIES.map((category) => (
+              <option key={category} value={category}>{category}</option>
+            ))
+          }
+        </select>
+        <label>Category</label>
       </div>
       <button type="submit" className="btn btn-primary col-lg-2 fw-bold">ADD BOOK</button>
     </form>
