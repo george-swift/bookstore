4994841f29b5e080b502b8906288375860309c91
George <71434803+george-swift@users.noreply.github.com>
2021-07-12 13:15:21 +0100

    Add remove book column and pass handler method to each book

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,23 +1,27 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeBook } from '../actions';
 import Book from '../components/Book';
 
 export default function BooksList() {
   const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  const handleRemoveBook = (book) => dispatch(removeBook(book));
 
   return (
-    <div className="container bg-light">
+    <div className="container bg-light table-responsive">
       <table className="table table-hover">
         <thead>
           <tr>
             <th scope="col">Book ID</th>
             <th scope="col">Title</th>
             <th scope="col">Category</th>
+            <th scope="col">Action</th>
           </tr>
         </thead>
         <tbody>
           {
             books.map((book) => (
-              <Book key={book.id} book={book} />
+              <Book key={book.id} book={book} removeBook={handleRemoveBook} />
             ))
           }
         </tbody>
