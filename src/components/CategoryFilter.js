6f67dbc104d52f10614d7685517feec1556d6ff6
George <71434803+george-swift@users.noreply.github.com>
2021-07-13 10:26:20 +0100

    Merge 'All' in categories

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -2,6 +2,8 @@ import PropTypes from 'prop-types';
 import { CATEGORIES } from '../constants';
 
 export default function CategoryFilter({ filterCategory }) {
+  const categories = [...['All'], ...CATEGORIES];
+
   const handleFilterChange = (e) => {
     const { value } = e.target;
     filterCategory(value);
@@ -12,9 +14,8 @@ export default function CategoryFilter({ filterCategory }) {
       className="form-select my-3 w-auto"
       onChange={handleFilterChange}
     >
-      <option value="All">All</option>
       {
-        CATEGORIES.map((category) => (
+        categories.map((category) => (
           <option key={category} value={category}>
             {category}
           </option>
