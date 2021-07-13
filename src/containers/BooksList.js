eda3c2692de645d94372b7e629fc534d339b6aee
George <71434803+george-swift@users.noreply.github.com>
2021-07-13 02:35:49 +0100

    Add category filter and pass the implemented handleFilterChange

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,24 +1,36 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeBook, filterByCategory } from '../actions';
+import { booksByCategory } from '../selectors';
+import CategoryFilter from '../components/CategoryFilter';
 import Book from '../components/Book';
 
 export default function BooksList() {
-  const books = useSelector((state) => state.books);
+  const books = useSelector((state) => booksByCategory(state));
+  const dispatch = useDispatch();
+  const handleRemoveBook = (book) => dispatch(removeBook(book));
+  const handleFilterChange = (filter) => dispatch(filterByCategory(filter));
 
   return (
-    <div className="container bg-light">
+    <div className="container bookslist">
+      <CategoryFilter filterCategory={handleFilterChange} />
       <table className="table table-hover">
         <thead>
           <tr>
             <th scope="col">Book ID</th>
             <th scope="col">Title</th>
             <th scope="col">Category</th>
+            <th scope="col">Action</th>
           </tr>
         </thead>
         <tbody>
           {
-            books.map((book) => (
-              <Book key={book.id} book={book} />
-            ))
+            books?.map((book) => <Book key={book.id} book={book} removeBook={handleRemoveBook} />)
+            ?? (
+              <tr>
+                <th scope="row">n/a</th>
+                <td colSpan="4">No available books in this category</td>
+              </tr>
+            )
           }
         </tbody>
       </table>
