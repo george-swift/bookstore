261dfd235f38afd8ddac32cb9842ce29e3717f51
George <71434803+george-swift@users.noreply.github.com>
2021-07-13 02:42:31 +0100

    Refactor to remove clutter

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,41 @@
 export const uniqueID = () => Math.floor(Math.random() * (16 ** 4));
 
 export const CATEGORIES = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+
+export const books = [
+  {
+    id: uniqueID(),
+    title: 'The Martian',
+    category: 'Sci-Fi',
+  },
+  {
+    id: uniqueID(),
+    title: 'Hands-on Machine Learning',
+    category: 'Learning',
+  },
+  {
+    id: uniqueID(),
+    title: "Alice's Adventures in Wonderland",
+    category: 'Kids',
+  },
+  {
+    id: uniqueID(),
+    title: 'The Woman in Black',
+    category: 'Horror',
+  },
+  {
+    id: uniqueID(),
+    title: 'Black and British: A Forgotten History',
+    category: 'History',
+  },
+  {
+    id: uniqueID(),
+    title: 'Steve Jobs',
+    category: 'Biography',
+  },
+  {
+    id: uniqueID(),
+    title: 'The Hunger Games',
+    category: 'Action',
+  },
+];
