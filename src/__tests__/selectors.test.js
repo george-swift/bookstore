db0dc884e3ee067ad63a91493de1dbd45e606888
George <71434803+george-swift@users.noreply.github.com>
2021-07-13 02:45:27 +0100

    Add test for selectors

---
+++ src/__tests__/selectors.test.js
@@ -0,0 +1,78 @@
+import { removeBook, filterByCategory } from '../actions';
+import books from '../reducers/books';
+import filter from '../reducers/filter';
+import { getBooks, getFilterCategory, booksByCategory } from '../selectors';
+import { uniqueID } from '../constants';
+
+describe('Testing BookStore selectors', () => {
+  const state = {
+    books: [
+      {
+        id: uniqueID(),
+        title: "Ender's game",
+        category: 'Sci-Fi',
+      },
+      {
+        id: uniqueID(),
+        title: 'Make it Stick',
+        category: 'Learning',
+      },
+    ],
+    filter: 'All',
+  };
+
+  afterEach(() => booksByCategory.resetRecomputations());
+
+  it('should retrieve books from the getBooks selector', () => {
+    expect(getBooks(state)).toEqual(state.books);
+  });
+
+  it('should retrieve category from the getFilterCategory selector', () => {
+    expect(getFilterCategory(state)).toMatch('All');
+  });
+
+  it('should return null if no books match a filter category', () => {
+    const action = filterByCategory('Biography');
+    const updatedFilter = filter(state.filter, action);
+    const newState = { ...state, filter: updatedFilter };
+    const booksByDefault = booksByCategory(state);
+
+    expect(booksByDefault).toHaveLength(2);
+
+    const newBooks = booksByCategory(newState);
+
+    expect(newBooks).toBeNull();
+  });
+
+  it('should recompute state when category filter changes', () => {
+    const action = filterByCategory('Learning');
+    const updatedFilter = filter(state.filter, action);
+    const newState = { ...state, filter: updatedFilter };
+
+    expect(booksByCategory.recomputations()).toEqual(0);
+    booksByCategory(state);
+    expect(booksByCategory.recomputations()).toEqual(1);
+    booksByCategory(newState);
+    expect(booksByCategory.recomputations()).toEqual(2);
+  });
+
+  it('should recompute state if book state in store changes', () => {
+    const action = removeBook(state.books[1]);
+    const updatedBooks = books(state.books, action);
+    const newState = { ...state, books: updatedBooks };
+
+    expect(booksByCategory.recomputations()).toEqual(0);
+    booksByCategory(state);
+    expect(booksByCategory.recomputations()).toEqual(1);
+    booksByCategory(newState);
+    expect(booksByCategory.recomputations()).toEqual(2);
+  });
+
+  it('should return a memoized object if store state is unchanged', () => {
+    const similarState = { ...state };
+    expect(booksByCategory.recomputations()).toEqual(0);
+    booksByCategory(state);
+    booksByCategory(similarState);
+    expect(booksByCategory.recomputations()).toEqual(1);
+  });
+});
