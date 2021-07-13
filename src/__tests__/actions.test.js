9e766d0224c6f24b9dd6cd5b6b0b438efc1051b7
George <71434803+george-swift@users.noreply.github.com>
2021-07-13 02:44:20 +0100

    Include filter action creator in actions test

---
+++ src/__tests__/actions.test.js
@@ -0,0 +1,39 @@
+import { createBook, removeBook, filterByCategory } from '../actions';
+import { uniqueID } from '../constants';
+
+describe('Testing action creators', () => {
+  const book = {
+    id: uniqueID(),
+    title: 'Test book',
+    category: 'Learning',
+  };
+
+  test('Should create book on submit', () => {
+    const expectedAction = {
+      type: 'CREATE_BOOK',
+      payload: book,
+    };
+
+    expect(createBook(book)).toEqual(expectedAction);
+  });
+
+  test('Should remove book on submit', () => {
+    const expectedAction = {
+      type: 'REMOVE_BOOK',
+      payload: {
+        bookId: book.id,
+      },
+    };
+
+    expect(removeBook(book)).toEqual(expectedAction);
+  });
+
+  test('Should provide a filter category', () => {
+    const expectedAction = {
+      type: 'CHANGE_FILTER',
+      payload: 'Sci-Fi',
+    };
+
+    expect(filterByCategory('Sci-Fi')).toEqual(expectedAction);
+  });
+});
