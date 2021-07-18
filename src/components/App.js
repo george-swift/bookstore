16c40605d3aa8d0c971125a28d528944e216baf4
George <71434803+george-swift@users.noreply.github.com>
2021-07-18 20:49:34 +0100

    Add routes to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,36 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import '../assets/Sass/App.scss';
+import { fetchBooksStarted } from '../actions';
+import FlashMessage from '../containers/FlashMessage';
 import Header from './Header';
-import BooksList from '../containers/BooksList';
-import BooksForm from '../containers/BooksForm';
+import Home from './Home';
+import BookEditor from '../containers/BookEditor';
+import NotFound from './NotFound';
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBooksStarted());
+  }, []);
+
   return (
-    <div className="app">
-      <Header />
-      <BooksList />
-      <BooksForm />
-    </div>
+    <Router>
+      <div className="app">
+        <FlashMessage />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/edit" element={<BookEditor />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
